012230d894a2d131c9f2209bb09fddbeef60c972
RoccoCerro <rcerro02@gmail.com>
2024-05-21 16:02:59 +0200

    Add: contenuto contatti

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -1,3 +1,68 @@
 @extends('layouts.blank')
 
-@section('main', 'Questi sono i contatti')
+@section('main')
+
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      <ul class="list-group">
+        <h1>Numeri di telefono</h1>
+        <li class="list-group-item">
+          <h3>Centralino</h3>
+          <div class="email">
+            +56 2568 2154
+          </div>
+        </li>
+        <li class="list-group-item">
+          <h3>Magazzino</h3>
+          <div class="email">
+            +56 2568 2154
+          </div>
+        </li>
+        <li class="list-group-item">
+          <h3>Addetto vendite</h3>
+          <div class="email">
+            +56 2568 2154
+          </div>
+        </li>
+        <li class="list-group-item">
+          <h3>Manutenzione</h3>
+          <div class="email">
+            +56 2568 2154
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="col-6">
+    <ul class="list-group">
+        <h1>Contatti email</h1>
+        <li class="list-group-item">
+          <h3>Centralino</h3>
+          <div class="email">
+            info@info.it
+          </div>
+        </li>
+        <li class="list-group-item">
+          <h3>Magazzino</h3>
+          <div class="email">
+            info@info.it
+          </div>
+        </li>
+        <li class="list-group-item">
+          <h3>Addetto vendite</h3>
+          <div class="email">
+            info@info.it
+          </div>
+        </li>
+        <li class="list-group-item">
+          <h3>Manutenzione</h3>
+          <div class="email">
+            info@info.it
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+@endsection
